539e4ad6406c0134011540f7c70dd1584138a8d0
Dominique Bashizi <dominique@blupods.com>
2016-09-28 10:42:41 -0400

    Latest changes

--- src/main/java/com/tiy/GameTrackerJSONController.java
+++ src/main/java/com/tiy/GameTrackerJSONController.java
@@ -1,10 +1,12 @@
 package com.tiy;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +16,29 @@ public class GameTrackerJSONController {
     @Autowired
     GameRepository games;
 
+    @RequestMapping(path = "/toggleGame.json", method = RequestMethod.GET)
+    public List<Game> toggleGame(int gameID) {
+        System.out.println("toggling game with ID " + gameID);
+        Game game = games.findOne(gameID);
+        game.name = "**" + game.name;
+        games.save(game);
+
+        return getGames();
+    }
+
+    @RequestMapping(path = "/addGame.json", method = RequestMethod.POST)
+    public List<Game> addGame(HttpSession session, @RequestBody Game game) throws Exception {
+        User user = (User)session.getAttribute("user");
+
+        if (user == null) {
+            throw new Exception("Unable to add game without an active user in the session");
+        }
+        game.user = user;
+
+        games.save(game);
+
+        return getGames();
+    }
 
     @RequestMapping(path = "/games.json", method = RequestMethod.GET)
     public List<Game> getGames() {
@@ -24,12 +49,14 @@ public class GameTrackerJSONController {
             gameList.add(game);
         }
 
-        try {
-            System.out.println("Catching a nap!");
-            Thread.sleep(3000);
-            System.out.println("Power naps are the best!!!");
-        } catch (Exception exception) {
-            exception.printStackTrace();
+        if (false) {
+            try {
+                System.out.println("Catching a nap!");
+                Thread.sleep(3000);
+                System.out.println("Power naps are the best!!!");
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
         }
 
         return gameList;
--- src/main/resources/static/js/tiy-ng-controller.js
+++ src/main/resources/static/js/tiy-ng-controller.js
@@ -1,11 +1,7 @@
 angular.module('TIYAngularApp', [])
    .controller('SampleController', function($scope, $http) {
-        $scope.name = "James";
-        $scope.testVar = {};
-        $scope.testVar.sampleContainerVar = "See, it's real!!!!";
-        $scope.testVar.anotherSample = 12;
-        $scope.testVar.flagExample = true;
-        $scope.testVar.somethingThatDoesntExist = "it does exist!!!";
+
+        console.log("I'm getting executed now!");
 
         $scope.getGames = function() {
             console.log("About to go get me some data!");
@@ -25,5 +21,54 @@ angular.module('TIYAngularApp', [])
             console.log("Done with the promise - waiting for the callback");
         };
 
+        $scope.toggleGame = function(gameID) {
+            console.log("About to toggle game with ID " + gameID);
+
+            $http.get("/toggleGame.json?gameID=" + gameID)
+                .then(
+                    function success(response) {
+                        console.log(response.data);
+                        console.log("Game toggled");
+
+                        $scope.games = response.data;
+                    },
+                    function error(response) {
+                        console.log("Unable to toggle game");
+                    });
+        };
+
+        $scope.addGame = function() {
+            console.log("About to add the following game " + JSON.stringify($scope.newGame));
+
+            $http.post("/addGame.json", $scope.newGame)
+                .then(
+                    function successCallback(response) {
+                        console.log(response.data);
+                        console.log("Adding data to scope");
+                        $scope.games = response.data;
+                    },
+                    function errorCallback(response) {
+                        console.log("Unable to get data");
+                    });
+        };
+
+        $scope.testFunction = function() {
+            console.log("Testing testing 123");
+            $scope.anotherFunction();
+        };
+
+        $scope.anotherFunction = function() {
+            console.log("anotherFunction()");
+        }
+
+        $scope.name = "James";
+        $scope.testVar = {};
+        $scope.testVar.sampleContainerVar = "See, it's real!!!!";
+        $scope.testVar.anotherSample = 12;
+        $scope.testVar.flagExample = true;
+        $scope.testVar.somethingThatDoesntExist = "it does exist!!!";
+
+        console.log("Initializing newGame now");
         $scope.newGame = {};
+
     });
